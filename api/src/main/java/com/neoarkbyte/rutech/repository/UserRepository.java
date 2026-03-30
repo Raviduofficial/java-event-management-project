6969affc93fec194efad1b0693c93f37288051dd
cw-miller <dylanwickramasinghe3@gmail.com>
2026-03-31 00:59:00 +0530

    - implemented CustomeUserDetailsService, UserPrincipal, UserRepository

--- api/src/main/java/com/neoarkbyte/rutech/repository/UserRepository.java
+++ api/src/main/java/com/neoarkbyte/rutech/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.neoarkbyte.rutech.repository;
 
-public class UserRepository {
+import com.neoarkbyte.rutech.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserName(String username);
 }
