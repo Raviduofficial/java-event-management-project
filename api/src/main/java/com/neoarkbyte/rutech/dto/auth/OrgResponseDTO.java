70bafb1143586c2d0518403828a14394eb217ca2
heshandilshara <heshandilshara9@gmail.com>
2026-04-21 14:49:11 +0530

    - modified the dtos and created all the necessary ones, fixed the secirity config, created the enums necessary

--- api/src/main/java/com/neoarkbyte/rutech/dto/auth/OrgResponseDTO.java
+++ api/src/main/java/com/neoarkbyte/rutech/dto/auth/OrgResponseDTO.java
@@ -2,29 +2,17 @@ package com.neoarkbyte.rutech.dto.auth;
 
 import com.neoarkbyte.rutech.type.ROLE;
 import lombok.Data;
-
 import java.util.Map;
 
 @Data
-public class UserCreateDTO {
-
+public class OrgResponseDTO{
+    private String userId;
     private String userName;
     private String name;
     private String email;
-    private String password;
     private String telephone;
     private String address;
     private ROLE role;
-
-    // Lecturer
-    private String department;
-    private String specialization;
-
-    // BatchRep
-    private String batchName;
-    private Integer year;
-
-    // Organization
     private Map<String, Object> committee;
     private String mission;
     private String vision;
